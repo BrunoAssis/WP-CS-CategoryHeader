11b8f8c37e38cfe4945fe38c5ea5e4f27c40d8c4
Bruno Assis <brunoassis@gmail.com>
2012-08-27 10:50:44 -0700

    Added options page, without the options.

--- WP-CS-CategoryHeader.php
+++ WP-CS-CategoryHeader.php
@@ -43,8 +43,12 @@ class WP_CS_CategoryHeader {
 	
 		// Register site styles
 		add_action( 'wp_enqueue_scripts', array( &$this, 'register_plugin_styles' ) );
-		
-	    add_filter( 'the_content', array( &$this, 'add_header' ) );
+
+		// Register main function.	
+		add_filter( 'the_content', array( &$this, 'add_header' ) );
+
+		// Register admin menu.
+		add_action( 'admin_menu', array( &$this, 'add_admin_menu' ) );
 
 	}
 	
@@ -77,7 +81,10 @@ class WP_CS_CategoryHeader {
 		wp_enqueue_style( 'WP-CS-CategoryHeader-plugin-styles' );
 	
 	}
-	
+
+	/**
+	 * Adds the main functionality of the plugin.
+	 */
 	public function add_header($content) {
 		$allowed_categories = array(1, 2, 3, 4, 5);
 		if ( in_category($allowed_categories) )
@@ -88,6 +95,20 @@ class WP_CS_CategoryHeader {
 		return $content;
 	}
 
+	public function add_admin_menu() {
+		add_plugins_page( 'CS Category Header Options', 'CS Category Header', 'manage_options', 'WP-CS-CategoryHeader', array(&$this, 'add_plugin_options') );
+	}
+
+	public function add_plugin_options() {
+		if ( !current_user_can( 'manage_options' ) ) {
+			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+		}
+		echo '<div class="wrap">';
+		echo '<p>Here is where the form would go if I actually had options.</p>';
+		echo '</div>';
+	}
+
 }
 
 new WP_CS_CategoryHeader();
+define( 'WP_DEBUG', true);
