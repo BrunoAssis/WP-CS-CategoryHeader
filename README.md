5a9d10666ce03c98361e30db3074b2be3490071c
Bruno Assis <brunoassis@gmail.com>
2012-08-27 14:13:14 +0000

    Added main plugin functionality and boilerplate code.

--- README.md
+++
@@ -1,4 +0,0 @@
-WP-CS-CategoryHeader
-====================
-
-Wordpress plugin used to display Category metadata in certain categories' posts.
---
+++ plugin.php
@@ -0,0 +1,104 @@
+<?php
+/*
+Plugin Name: CS-CategoryHeader
+Plugin URI: http://github.com/BrunoAssis/WP-CS-CategoryHeader
+Description: Inserts some categories' descriptions in content header.
+Version: 0.1
+Author: Bruno Assis
+Author URI: http://brunoassis.org
+Author Email: brunoassis@gmail.com
+License:
+
+  Copyright 2012 Bruno Assis (brunoassis@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+*/
+
+class CSCategoryHeader {
+	 
+	/*--------------------------------------------*
+	 * Constructor
+	 *--------------------------------------------*/
+	
+	/**
+	 * Initializes the plugin by setting localization, filters, and administration functions.
+	 */
+	function __construct() {
+	
+		// Register admin styles and scripts
+		add_action( 'admin_print_styles', array( &$this, 'register_admin_styles' ) );
+		add_action( 'admin_enqueue_scripts', array( &$this, 'register_admin_scripts' ) );
+	
+		// Register site styles
+		add_action( 'wp_enqueue_scripts', array( &$this, 'register_plugin_styles' ) );
+		
+	    /*
+	     * TODO:
+	     * Define the custom functionality for your plugin. The first parameter of the
+	     * add_action/add_filter calls are the hooks into which your code should fire.
+	     *
+	     * The second parameter is the function name located within this class. See the stubs
+	     * later in the file.
+	     *
+	     * For more information: 
+	     * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
+	     */
+	    add_filter( 'the_content()', array( $this, 'add_header()' ) );
+
+	}
+	
+	/**
+	 * Registers and enqueues admin-specific styles.
+	 */
+	public function register_admin_styles() {
+	
+		wp_register_style( 'cs-category-header-admin-styles', plugins_url( 'cs-category-header/css/admin.css' ) );
+		wp_enqueue_style( 'cs-category-header-admin-styles' );
+	
+	}
+
+	/**
+	 * Registers and enqueues admin-specific JavaScript.
+	 */	
+	public function register_admin_scripts() {
+	
+		wp_register_script( 'cs-category-header-admin-script', plugins_url( 'cs-category-header/js/admin.js' ) );
+		wp_enqueue_script( 'cs-category-header-admin-script' );
+	
+	}
+	
+	/**
+	 * Registers and enqueues plugin-specific styles.
+	 */
+	public function register_plugin_styles() {
+	
+		wp_register_style( 'cs-category-header-plugin-styles', plugins_url( 'cs-category-header/css/display.css' ) );
+		wp_enqueue_style( 'cs-category-header-plugin-styles' );
+	
+	}
+	
+	public function add_header($content) {
+		$allowed_categories = array(1, 2, 3, 4, 5);
+		if ( is_single() && true )
+			$content = sprintf(
+				'CAIXINHA_AQUI%s',
+				$content
+			);
+		return $content;
+	}
+
+}
+
+new CSCategoryHeader();
